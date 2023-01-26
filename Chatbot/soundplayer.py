19f94e14953f38470a4e21c12e9994a70bf40329
Yau Shing <75722100+yaushing@users.noreply.github.com>
2023-01-26 15:13:11 +0800

    Lol now chatbot just needs training data
    added syllable files - for 11000 syllables

--- Chatbot/soundplayer.py
+++ Chatbot/soundplayer.py
@@ -1,21 +1,7 @@
 from playsound import playsound
 import time, random
-from tqdm import tqdm as tqdm
 from splitter import hyphenate_word
-from makesound import createsound
 import os
-
-def get_random_permutation(alrused, datalist):
-        chosen = []
-        used = []
-        chosen = [random.choice(datalist), random.choice(datalist)]
-        while chosen in alrused:
-                chosen = [random.choice(datalist), random.choice(datalist)]
-                if chosen not in alrused:
-                        break
-        used = alrused
-        used.append(chosen)                  
-        return used, chosen
 
 def transsent(sentence):
         sdir = "../Chatbot/sylsound/"
@@ -37,23 +23,13 @@ def transsent(sentence):
                 if (syl.lower() + ".wav") in sounds:
                         playsound(sdir + syl.lower() + ".wav")
                 else:
-                        sounds = os.listdir(sdir)
+                        sounds = os.listdir(sdirb)
                         for i in range(len(sounds)):
                                 if "unnamed" in sounds[i]:
-                                        oldfdir = f"{sdir}{sounds[i]}"
+                                        oldfdir = f"{sdirb}{sounds[i]}"
                                         newfdir = f"{sdir}{syl.lower()}.wav"
                                         os.rename(oldfdir, newfdir)
                                         playsound(newfdir)
                                         break
-                                else:
-                                        base = os.listdir(sdirb)
-                                        data = []
-                                        perm = get_random_permutation(data, base)
-                                        data = perm[0]
-                                        chosensounds = perm[1]
-                                        sortedperm = []
-                                        for sound in chosensounds:
-                                                sortedperm.append(f"{sdirb}{sound}")
-                                        createsound(sortedperm, f"{syl.lower()}.wav")
-                        playsound(sdir + syl.lower() + ".wav")
                                 
+transsent("hello there")
